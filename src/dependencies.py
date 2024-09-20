5331dcd33e2557069c007de8b84d1129a0768677
d-pamneja <dpamneja@gmail.com>
2024-09-20 18:38:09 +0530

    streamlit cloud deployment

--- src/dependencies.py
+++ src/dependencies.py
@@ -8,9 +8,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 from openai import OpenAI
 from dotenv import load_dotenv
 
+load_dotenv()
+print(os.environ.get("OPENAI_API_KEY"))
 import streamlit as st
 
-os.environ.pop("OPENAI_API_KEY", None)
 load_dotenv()
 KEY = os.getenv("OPENAI_API_KEY")
 print(KEY)
